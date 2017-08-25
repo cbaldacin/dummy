bd7b6545653ff5ec55f1831d1186e089f41ca1f3
cbaldacin-allpago <carlos.baldacin@allpago.com>
2017-08-25 18:50:39 +0200

    log

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -2,6 +2,8 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/boleto-check")
 public class HelloWorldController {
+    
+    private static final Logger log = LoggerFactory.getLogger(HelloWorldController.class);
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
@@ -18,6 +22,9 @@ public class HelloWorldController {
     @RequestMapping(method=RequestMethod.GET)
     public @ResponseBody Greeting check(@RequestParam(value="numero_pedido", required=false, defaultValue="Stranger") String orderNumber,
             @RequestParam(value="token", required=true) String token) {
+        
+        log.debug("Order {} Token {}", orderNumber, token);
+        
         return new Greeting(counter.incrementAndGet(), String.format(template, orderNumber+":"+token));
     }
 
